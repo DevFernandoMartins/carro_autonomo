fb20c74f3fedeca86311aba2600912b6f9691bf0
Fernando Martins <fe.092006@gmail.com>
2024-04-23 15:32:55 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Carro Autônomo SENAC
+O projeto do carro autônomo SENAC, veio a partir da **[3ª Competição Brasileira de Veículos Autônomos em Escala](https://www.robocarrace.com.br/)**, tendo o seu público estudantes, makers, empresas, pesquisadores e entusiastas.
+
+## Como utilizar
+ 
+Baixe os arquivos usando o comando seguinte no seu terminal
+```shell
+git clone https://github.com/DevFernandoMartins/carro_autonomo.git
+ ```
+
+ ## Contribuindo
+
+Contribuições são bem-vindas! Se você encontrar algum problema ou tiver sugestões para melhorias, por favor, entre em contato via instagram [@DevFernandoMartins](https://instagram.com/DevFernandoMartins)
+
+##
+#### Copyright © 2023 / [Fernando Martins](https://github.com/DevFernandoMartins)
+
+A permissão é concedida, gratuitamente, a qualquer pessoa que obtenha uma cópia deste arquivo, sem restrição nos direitos de usar, copiar, modificar ou mesclar.
---
+++ main_pc.py
@@ -0,0 +1,49 @@
+# Software utilizado para a camera: Iriun WebCam
+
+#from time import sleep
+import cv2
+import numpy as np
+
+motor_e = 0
+motor_d = 0
+
+centro_tela = 320
+
+camera = cv2.VideoCapture(0)
+print("Camera Iniciada")
+
+while True:
+    ret, frame = camera.read()
+
+    frame = cv2.GaussianBlur(frame,(5,5),0)
+    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    cv2.imshow('HSV', hsv)
+    
+    lower_y = np.array([10,26,91]) # Preto
+    upper_y = np.array([175,175,175]) # Branco
+
+    mask = cv2.inRange(hsv,lower_y, upper_y)
+    edges = cv2.Canny(mask,74,150)
+    lines = cv2.HoughLinesP(edges,1,np.pi/180,30,maxLineGap = 30)
+    
+    if lines is not None:
+        for line in lines:
+            x1,y1,x2,y2 = line[0]
+            centro_linha = (x1 + x2) / 2
+            cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),5)
+    
+            if centro_linha < (centro_tela + 50):
+                print("Esquerda <---") 
+            elif centro_linha > (centro_tela - 50):
+                print("Direita --->")
+            else:
+                print("Reto ")
+                
+        cv2.imshow('Imagem', frame)
+        cv2.imshow('Borda', edges)
+       
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+cv2.destroyAllWindows()
+exit()
